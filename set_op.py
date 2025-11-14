d734bd31605b8c3aa0805baa9d0c22b7a5afeab5
Charan-suresh <charansuresh456@gmail.com>
2025-11-14 09:38:51 +0530

    Implement set operations for sets E and N

---
+++ set_op.py
@@ -0,0 +1,14 @@
+
+E = {0, 2, 4, 6, 8}  
+N = {1, 2, 3, 4, 5}  
+
+
+union = E | N
+intersection = E & N
+difference = E - N
+symmetric_difference = E ^ N
+
+print(f"Union of E and N is {union}")
+print(f"Intersection of E and N is {intersection}")
+print(f"Difference of E and N is {difference}")
+print(f"Symmetric difference of E and N is {symmetric_difference}")
